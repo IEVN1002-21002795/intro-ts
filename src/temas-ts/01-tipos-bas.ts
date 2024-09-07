c70f5c358a5ff87e7358424ca8ab6074cd685e46
Diego Armando Hernandez Trujillo <diegotrujillo1899@gamil.com>
2024-09-07 10:08:08 -0600

    primer commit intro-ts

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,15 @@
+
+var num1=23;
+let num2='Juan';
+const num3=34;
+
+const sum = num1 + num2 + num3;
+console.log(`The sum is: ${sum}`);
+
+let nombre:string='juan';
+let edad:number=23;
+let activo:boolean=true;
+
+let matricula:number|string;
+matricula=21002795;
+matricula='dieguito21002795'
